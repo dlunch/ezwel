3526b8ca9ba1fd6f3459b3afaec3cc9082960699
Inseok Lee <git@dlun.ch>
2022-01-13 20:33:20 +0900

    Send initial requets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = reqwest::Client::builder().cookie_store(true).build()?;
+
+    // set cookie
+    client
+        .get("https://sendbird.ezwel.com/cuser/login/loginForm.ez")
+        .send()
+        .await?;
+
+    // login
+
+    Ok(())
 }
