400b2943795a9838fe2a4bce87e896f33e42270f
Inseok Lee <git@dlun.ch>
2022-01-14 10:59:47 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,7 @@ async fn main() -> Result<()> {
     let client = reqwest::Client::builder().cookie_store(true).build()?;
 
     // set cookie
-    client
-        .get("https://sendbird.ezwel.com/cuser/login/loginForm.ez")
-        .send()
-        .await?;
+    client.get("https://sendbird.ezwel.com/cuser/login/loginForm.ez").send().await?;
 
     // login
 
